c4ea5c1fe2458aab2868db64d7447687eec6467f
Thomas Johnson <tjohnson1106@gmail.com>
2017-06-13 12:04:24 -0400

    added mocha test package to test base code before altering view layer

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -7,7 +7,12 @@ export const Tasks = new Mongo.Collection('tasks');
 if (Meteor.isServer) {
   // This code only runs on the server
   Meteor.publish('tasks', function tasksPublication() {
-    return Tasks.find();
+     return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
   });
 }
 
@@ -29,6 +34,12 @@ Meteor.methods({
   },
   'tasks.remove'(taskId) {
     check(taskId, String);
+
+     const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized');
+    }
  
     Tasks.remove(taskId);
   },
